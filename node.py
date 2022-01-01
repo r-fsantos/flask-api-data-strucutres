55198692e103b344835cdf30947328a2ba732c32
renato <felicio95.jr@gmail.com>
2022-01-01 06:57:22 -0300

    :truck: Moving Node class from linked list to stand-alone node file.

---
+++ node.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from typing import Any
+
+
+class Node:
+	"""
+	Add Doc Strings
+	"""
+
+	_data: Any
+	_next_node: None  # can be Node as well
+
+	def __init__(
+		self,
+		data: Any,
+		next_node: None  # can be Node as well
+	) -> None:
+		self._data: Any = data
+		self._next_node: Node = next_node
+	
+	def set_data(self, data: Any) -> None:
+		self._data = data
+	
+	def get_data(self) -> Any:
+		return self._data
+	
+	def set_next_node(self, node: None) -> None:
+		self._next_node = node
+	
+	def get_next_node(self):
+		return self._next_node
