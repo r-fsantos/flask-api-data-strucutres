e92f25fe497dd61a5b6c9930e08a885ef5b95f67
renato <felicio95.jr@gmail.com>
2022-01-09 17:28:03 -0300

    :recycle: :hammer: BinarySearchTree refactoring and adding new methods to it.
        - Insertion method parameters names are now data, not value anymore.
        It becomes more readable since it can be data structures like dicts and lists.
        - Adding search and recursive_searching methods. This implementation
        highly depends on dict implementation.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -20,23 +20,47 @@ class BinarySearchTree:  # BST
 	def __init__(self) -> None:
 		self.root: Node = None
 	
-	def insert(self, value: Any) -> None:
+	def insert(self, data: Any) -> None:
 		if self.root is None:  # empty tree
-			self.root = Node(data=value)
+			self.root = Node(data=data)
 		else:  # root is not none. Now, recursively adds data
-			self._recursive_insertion(value=value, node=self.root)
+			self._recursive_insertion(data=data, node=self.root)
 	
-	def _recursive_insertion(self, value: Any, node: Node) -> None:
-		if value < node.data:
+	def _recursive_insertion(self, data: Any, node: Node) -> None:
+		if data["id"] < node.data["id"]:
 			if node.left is None:
-				node.left = Node(data=value)
+				node.left = Node(data=data)
 			else:
-				self._recursive_insertion(value=value, node=node.left)
-		elif value > node.data:
+				self._recursive_insertion(data=data, node=node.left)
+		elif data["id"] > node.data["id"]:
 			if node.right is None:
-				node.right = Node(data=value)
+				node.right = Node(data=data)
 			else:
-				self._recursive_insertion(value=value, node=node.right)
-		else:  # value == node.data.
+				self._recursive_insertion(data=data, node=node.right)
+		else:  # data == node.data
 			return
 	
+	def search(self, id: int) -> dict:
+		_id: int = int(id)
+
+		if self.root is None:
+			return False
+		
+		return self._recursive_searching(id=_id, node=self.root)
+	
+	def _recursive_searching(self, id: int, node: Node) -> dict:		
+		if id == node.data["id"]:
+			return node.data
+		
+		if id < node.data["id"] and node.left is not None:
+			if id == node.left.data["id"]:
+				return node.left.data
+			return self._recursive_searching(id=id, node=node.left)
+
+		if id > node.data["id"] and node.right is not None:
+			if id == node.right.data["id"]:
+				return node.right.data
+			return self._recursive_searching(id=id, node=node.right)
+
+		return {}
+
